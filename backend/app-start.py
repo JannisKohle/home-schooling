ce0482b8dff099e0059643c869e39b66df49eceb
JannisKohle <Jannis.Kohle@icloud.com>
2020-04-27 17:24:17 +0200

    created backend folder and app-start.py

---
+++ backend/app-start.py
@@ -0,0 +1,15 @@
+class App():
+    def __init__(self):
+        pass
+
+    def check(self):  # return True if logged in, otherwise return False
+        pass
+
+    def log_in(self, username, password):  # return True if it works, otherwise False
+        pass
+
+    def log_out(self, confirmation):  # return True if confirmation==True and it worked, otherwise False
+        pass
+
+    def sign_up(self, username, password):  # return True if it works, otherwise False
+        pass
